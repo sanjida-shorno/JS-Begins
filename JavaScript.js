9de92df20f5c604f31d8b679365cef76b22578f3
sanjida-shorno <tasnimshorno@gmail.com>
2022-11-01 18:59:07 +0600

    arrow function

--- JavaScript.js
+++ JavaScript.js
@@ -16,7 +16,7 @@ console.log(fruitProcessor(10, 20));
 const drink = fruitProcessor(5, 8);
 console.log(drink);
 */
-
+/*
 //function declaration
 
 function calcAge1(bithYear) {
@@ -45,7 +45,7 @@ const age3 = calcAge3(1995);
 console.log(age3);
 
 console.log(age1, age2, age3);
-
+*/
 //calling the function first in function expression will not give any output.
 
 /*
@@ -56,4 +56,54 @@ const calcAge3 = function (bithYear) {
     return 2030 - bithYear;
 }
 */
+
+//arrow funtion 
+//for one parameter and one line of code
+
+const calcAge4 = birthYear => 2022 - birthYear;
+console.log(calcAge4(1995));
+
+//using variable to store the value
+
+const age = calcAge4(1997);
+console.log(age);
+
+
+
+
+//for one parameter and multiple lines of code
+
+const ageUntilRetirement = birthYear => {
+    const age = 2022 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+
+}
+
+console.log(ageUntilRetirement(1995));
+
+//using variable to store the value
+
+const yearsLeft = ageUntilRetirement(1997);
+console.log(yearsLeft);
+
+
+
+
+//for multiple parameter and multiple lines of code
+
+
+const ageUntilRetirement1 = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    return `${firstName} has ${65 - age} years`;
+
+}
+
+console.log(ageUntilRetirement1(1995, 'shorno'));
+
+//using variable to store the value
+
+const yearsLeft1 = ageUntilRetirement1(1997, 'dhonno');
+console.log(yearsLeft1);
+
 
