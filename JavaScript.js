c80d239c9781ce88cfdd917c1b71405b46d2938c
sanjida-shorno <tasnimshorno@gmail.com>
2022-11-02 12:04:20 +0600

    coding challenge1(f3)

--- JavaScript.js
+++ JavaScript.js
@@ -162,7 +162,7 @@ console.log(drinkJuice2);
 
 
 
-//function calling unsing arrow function 
+//function calling unsing arrow function
 
 const cutFruitPieces3 = fruit => fruit * 4;
 
@@ -188,7 +188,7 @@ console.log(drinkJuice3);
 
 //function review
 
-
+/*
 const calcAge = function (birthyear) {
     return 2022 - birthyear;
 
@@ -210,3 +210,61 @@ const yearsUntilretirement = function (birthYear, firstName) {
 
 console.log(yearsUntilretirement(1995, 'shorno'));
 console.log(yearsUntilretirement(1950, 'dhonno'));
+*/
+
+//coding challenge #1(fundamentals part2)
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§
+§
+Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores 😉
+*/
+
+const calcAverage = (value1, value2, value3) => (value1 + value2 + value3) / 3;
+
+const avgDolphins = calcAverage(44, 23, 71);
+console.log(avgDolphins);
+
+const avgKoalas = calcAverage(65, 54, 49);
+console.log(avgKoalas);
+
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+
+    } else {
+        console.log("no team wins")
+    }
+}
+
+checkWinner(avgDolphins, avgKoalas);
+
+const scoreDolphins = calcAverage(85, 54, 41,);
+const scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+checkWinner(scoreDolphins, scoreKoalas);
